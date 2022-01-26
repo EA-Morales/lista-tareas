707228cca46974b059643495a0d8cf7bd694edf7
l3gol4s <eduardo.agustin.morales@gmail.com>
2022-01-26 15:47:20 -0300

    service observable set
    todo funciona correctamente

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-// Interface & Lista
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../service/task.service';
 import { Task } from '../../task';
 
 @Component({
@@ -10,9 +9,13 @@ import { Task } from '../../task';
   styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+// Interface & Mockup
+import { Task } from '../task';
+import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
   constructor() {}
+
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
+  }
 }
